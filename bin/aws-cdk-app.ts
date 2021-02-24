34762b96288da9d64ea769c8b85229a9558dfbba
kkovalchuk <kateryna.kovalchuk@atip.de>
2021-02-24 18:50:30 +0100

    widgets added

--- bin/aws-cdk-app.ts
+++ bin/aws-cdk-app.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { AwsCdkAppStack } from '../lib/aws-cdk-app-stack';
+import { WidgetService } from '../lib/widget-service';
 
 const app = new cdk.App();
 new AwsCdkAppStack(app, 'AwsCdkAppStack');
+//new WidgetService.WidgetService(this, 'Widgets');
