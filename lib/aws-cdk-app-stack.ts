43f2a462983ea4cf76f469fe04ffd6d57f48358b
kkovalchuk <kateryna.kovalchuk@atip.de>
2021-02-24 18:00:31 +0100

    app config

---
+++ lib/aws-cdk-app-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda'
+
+const { ApiGatewayToLambda } = require('@aws-solutions-constructs/aws-apigateway-lambda');
+
+export class AwsCdkAppStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+
+    new ApiGatewayToLambda(this, 'ApiGatewayToLambdaPattern', {
+        lambdaFunctionProps: {
+            runtime: lambda.Runtime.NODEJS_10_X,
+            handler: 'index.handler',
+            code: lambda.Code.fromAsset(`${__dirname}/lambda`)
+        }
+    });
+  }
+}
